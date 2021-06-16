e6c32561b6135f7de2a0daee6cda193e4a547a09
mrmorgano001 <mrmorgano001@gmail.com>
2021-06-16 11:54:44 +0300

    Lesson-1 ConsoleApp added

---
+++ lesson-1/HelloUserName/HelloUserName/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloUserName
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            var userName = Console.ReadLine();
+            var dateTime = DateTime.Now;
+            Console.WriteLine($"Привет {userName}, сейчас {dateTime} !");
+            Console.ReadKey();
+        }
+    }
+}
