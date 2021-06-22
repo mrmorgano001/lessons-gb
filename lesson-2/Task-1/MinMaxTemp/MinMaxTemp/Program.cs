5955d44e0928d769431878bd4fe9f638338a1146
mrmorgano001 <mrmorgano001@gmail.com>
2021-06-22 08:22:23 +0300

    Added Task-1 to Lesson-2

---
+++ lesson-2/Task-1/MinMaxTemp/MinMaxTemp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MinMaxTemp
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+    Console.WriteLine("Введите максимальную температуру: ");
+    var max = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальную температуру: ");
+    var min = Convert.ToInt32(Console.ReadLine());
+    int average = (min + max) / 2;
+    Console.WriteLine($"Средняя температура за сутки: {average}");
+    Console.ReadKey();
+    }
+  }
+}
