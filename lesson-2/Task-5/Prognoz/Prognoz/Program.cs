fec9ac422bbd8ff651adea9b490bb153f395830e
mrmorgano001 <mrmorgano001@gmail.com>
2021-06-22 18:40:55 +0300

    Corrected Task-5 Lesson-2

--- lesson-2/Task-5/Prognoz/Prognoz/Program.cs
+++ lesson-2/Task-5/Prognoz/Prognoz/Program.cs
@@ -6,74 +6,70 @@ namespace Prognoz
     {
         public static void Main(string[] args)
         {
-            try
+            int average = 0;
+            Console.Write("Введите порядковый номер месяца: ");
+            if (Int32.TryParse(Console.ReadLine(), out int month))
             {
-                int max = 0;
-                int min = 0;
-                int average = 0;
-                Console.Write("Введите порядковый номер месяца: ");
-                var month = Convert.ToInt32(Console.ReadLine());
                 if (month == 1 || month == 2 || month == 12)
                 {
                     Console.Write("Введите максимальную температуру: ");
-                    max = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Введите минимальную температуру: ");
-                    min = Convert.ToInt32(Console.ReadLine());
-                    average = (min + max) / 2;
+                    int max = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите минимальную температуру: ");
+                    int min = Convert.ToInt32(Console.ReadLine());
+                    average = (max + min) / 2;
                 }
-                
-                if (month == 1)
-                {
-                    Console.WriteLine("Январь");
-                    if (average > 0)
+                switch (month)
                     {
-                        Console.WriteLine("Дождливая зима");
+                        case 1:
+                            Console.WriteLine("Январь");
+                            if (average > 0)
+                                Console.WriteLine("Дождливая зима");
+                            break;
+                        case 2:
+                            Console.WriteLine("Февраль");
+                            if (average > 0)
+                                Console.WriteLine("Дождливая зима");
+                            break;
+                        case 3:
+                            Console.WriteLine("Март");
+                            break;
+                        case 4:
+                            Console.WriteLine("Апрель");
+                            break;
+                        case 5:
+                            Console.WriteLine("Май");
+                            break;
+                        case 6:
+                            Console.WriteLine("Июнь");
+                            break;
+                        case 7:
+                            Console.WriteLine("Июль");
+                            break;
+                        case 8:
+                            Console.WriteLine("Август");
+                            break;
+                        case 9:
+                            Console.WriteLine("Сентябрь");
+                            break;
+                        case 10:
+                            Console.WriteLine("Октябрь");
+                            break;
+                        case 11:
+                            Console.WriteLine("Ноябрь");
+                            break;
+                        case 12:
+                            Console.WriteLine("Декабрь");
+                            if (average > 0)
+                                Console.WriteLine("Дождливая зима");
+                            break;
+                        default:
+                            Console.WriteLine("Такого месяца нет");
+                            break;
                     }
                 }
-                else if (month == 2)
-                {
-                    Console.WriteLine("Февраль");
-                    if (average > 0)
-                    {
-                        Console.WriteLine("Дождливая зима");
-                    }
-                }
-                else if (month == 3)
-                    Console.Write("Март");
-                else if (month == 4)
-                    Console.Write("Апрель");
-                else if (month == 5)
-                    Console.Write("Май");
-                else if (month == 6)
-                    Console.Write("Июнь");
-                else if (month == 7)
-                    Console.Write("Июль");
-                else if (month == 8)
-                    Console.Write("Август");
-                else if (month == 9)
-                    Console.Write("Сентябрь");
-                else if (month == 10)
-                    Console.Write("Октябрь");
-                else if (month == 11)
-                    Console.Write("Ноябрь");
-                else if (month == 12)
-                {
-                    Console.WriteLine("Декабрь");
-                    if (average > 0)
-                    {
-                        Console.WriteLine("Дождливая зима");
-                    }
-                }
-                else 
-                    Console.Write("Введите корректный номер месяца");
-                
+            else
+                    Console.WriteLine("Вы ввели не число");
+                Console.ReadKey();
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadKey();
-
         }
-    }
 }
