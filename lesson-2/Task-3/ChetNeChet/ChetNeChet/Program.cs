d9c93e61bee9fdd5a230586a3823739766db87c5
mrmorgano001 <mrmorgano001@gmail.com>
2021-06-22 08:40:39 +0300

    Added Task-3 To Lesson-2

---
+++ lesson-2/Task-3/ChetNeChet/ChetNeChet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChetNeChet
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число четное.");
+            }else 
+                Console.WriteLine("Число нечетное.");
+
+            Console.ReadKey();
+        }
+    }
+}
