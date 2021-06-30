6695f6ab097ade9ef05970f0192d72b0bd2dd586
mrmorgano001 <mrmorgano001@gmail.com>
2021-06-30 13:21:19 +0300

    Addedd Lesson4

---
+++ lesson-4/Task-1/Task-1/GetUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task_1
+{
+    public class GetUser
+    {
+        public static int GetInfo(int count)
+        {
+            return count;
+        }
+
+        public static string GetGullName(string firstName, string lastName, string patronymic)
+        {
+            return $"{firstName} + {lastName} + {patronymic}";
+        }
+    }
+}
