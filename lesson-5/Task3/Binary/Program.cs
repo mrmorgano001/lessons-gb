77840d284193bd721a0558117fd0c3df9a628c75
mrmorgano001 <mrmorgano001@gmail.com>
2021-07-01 16:46:11 +0300

    Addedd Task-3

---
+++ lesson-5/Task3/Binary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите произвольный набор чисел от 0 до 255");
+            string numbers = Console.ReadLine();
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(new FileStream("numbers.bin", FileMode.OpenOrCreate), numbers);
+        }
+    }
+}
