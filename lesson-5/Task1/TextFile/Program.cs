d12d15dbbb0c7757aeae9078d12e000896a479b0
mrmorgano001 <mrmorgano001@gmail.com>
2021-07-01 16:35:48 +0300

    Addedd Task-1,Task-2

---
+++ lesson-5/Task1/TextFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите произвольные данные: ");
+            string line = Console.ReadLine();
+            File.WriteAllText("text.txt", line);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson-5/Task2/Write_startup/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace Write_startup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "text.txt";
+            Console.WriteLine("На данный момент системное время: " + DateTime.Now.ToString("HH:mm:ss"));
+            File.AppendAllText(filename, $"{Environment.NewLine + DateTime.Now:HH:mm:s}");
+        }
+    }
+}
