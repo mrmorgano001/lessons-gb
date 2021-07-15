7a752086e5ed68feddd5d302f6bf9ef1edda28ff
mrmorgano001 <mrmorgano001@gmail.com>
2021-07-15 11:48:03 +0300

    Version-1

---
+++ lesson-9/FileManager/FileManager/Functions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace FileManager
+{
+    public class Functions
+    {
+        public static void Header()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("┌─────────────────────────────────────────────────────FILE-MANAGER───────────────────────────────────────────────────┐");
+            Console.WriteLine("│ COMMANDS:                                                                                                          │");
+            Console.WriteLine("│ ls <path> - просмотр файлов каталога       cp <path1> <path2> - копирование файла (каталога)                       │");
+            Console.WriteLine("│                                                                                                                    │");
+            Console.WriteLine("│ rm <path> - удаление каталога(рекурсивно)/файла    file <file> - вывод информации о файле                          │");
+            Console.WriteLine("│ введите quitя выхода                                                                                               │");
+            Console.WriteLine("└────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘");
+            Console.SetWindowSize(120,30);
+        }
+        public static void DirectoryViewer(string currentDirectory)
+        {
+            Console.Clear();
+            Header();
+            var viewDirectories = Directory.GetDirectories(currentDirectory);
+            foreach (var directoryName in viewDirectories)
+            {
+                var currentDir = directoryName; 
+                Console.WriteLine("──" + directoryName);
+            }
+        }
+
+        public static void FileViewer(string targetDirectory)
+        {
+            var getFiles = Directory.GetFiles(targetDirectory);
+            foreach (var filename in getFiles)
+            {
+                Console.WriteLine("   └──" + filename);
+            }
+        }
+        public static void KeyHandler(string command)
+        {
+            string[] subs = command.Split(" ");
+            if (command != String.Empty)
+            {
+                if (subs[0].ToUpper() == "LS")
+                    DirectoryViewer(subs[1]);
+                if (subs[0].ToUpper() == "CP")
+                    CopyFile(subs[1], subs[2]);
+                if (subs[0].ToUpper() == "RM")
+                    DeleteFile(subs[1]);
+            }
+            else
+            {
+                Console.WriteLine("Команда не может быть пустая.");
+            }
+
+        }
+        private static void CopyFile(string firstCatalog, string secondCatalog)
+        {
+            File.Copy(firstCatalog,secondCatalog);
+        }
+        private static void DeleteFile(string filename)
+        {
+            File.Delete(filename);
+        }
+    }
+}
---
+++ lesson-9/FileManager/FileManager/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var command = string.Empty;
+            Functions.Header(); 
+            var currentDirectory = @"C:\";
+           Functions.DirectoryViewer(currentDirectory);
+           Console.WriteLine("Введите команду: ");
+           while (command.ToUpper() != "QUIT")
+           {
+               command = Console.ReadLine();
+               Functions.KeyHandler(command);
+           }
+        }
+    }
+}
